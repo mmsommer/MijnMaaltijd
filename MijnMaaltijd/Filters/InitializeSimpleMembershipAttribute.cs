1f55ca4cd4608d75baf050857e60605a2d9afafe
Michael Sommer <michael.sommer@ogd.nl>
2014-06-30 16:56:09 +0200

    Corrupted the username/password

--- MijnMaaltijd/Filters/InitializeSimpleMembershipAttribute.cs
+++ MijnMaaltijd/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace MijnMaaltijd.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("Entities", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
